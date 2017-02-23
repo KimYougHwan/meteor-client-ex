892bc5ba32de70e52dbae840ee1029cc91f561d1
ROKHYBRID <hybrid3397@daum.net>
2017-02-23 17:04:50 +0900

    전체 레이아웃 작성 (모든 메뉴 뷰 및 로그인 뷰)

--- client/index/B_gradesAnalysis/B_A_schoolRecordsAnalysis/B_A_B_ratingAnalysis/B_A_B_ratingAnalysis.js
+++ client/index/B_gradesAnalysis/B_A_schoolRecordsAnalysis/B_A_B_ratingAnalysis/B_A_B_ratingAnalysis.js
@@ -5,3 +5,29 @@ Template.B_A_B_ratingAnalysis.helpers({
 Template.B_A_B_ratingAnalysis.events({
 	
 });
+
+Template.B_A_B_ratingAnalysis.onRendered(function () {
+	var chart1 = Highcharts.chart('chart1', {
+		chart: {
+			type: 'bar'
+		},
+		title: {
+			text: 'Fruit Consumption'
+		},
+		xAxis: {
+			categories: ['Apples', 'Bananas', 'Oranges']
+		},
+		yAxis: {
+			title: {
+				text: 'Fruit eaten'
+			}
+		},
+		series: [{
+			name: 'Jane',
+			data: [1, 0, 4]
+		}, {
+			name: 'John',
+			data: [5, 7, 3]
+		}]
+	});
+});
